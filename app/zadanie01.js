adaaaf23e455a829e423ecb92cf5845826bd29d4
azzz90 <aleksander.ziolkowski90@gmail.com>
2018-02-04 23:59:45 +0100

    zad1 czesciowo

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,10 @@
 //Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static('./public/zadanie01/'));
+
+app.listen(3000,()=>{
+  console.log('Serwer działa na porcie 3000');
+})
