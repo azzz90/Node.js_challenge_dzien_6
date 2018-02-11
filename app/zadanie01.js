7c4aa520629a41f809c9759dbd5a2dd5e452eaa0
azzz90 <aleksander.ziolkowski90@gmail.com>
2018-02-11 23:40:53 +0100

    Zad 1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -3,7 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded());
 app.use(express.static('./public/zadanie01/'));
+
+app.post('/form', (req, res) => {
+    const {numOne, numTwo} = req.body;
+    //res.send('Pierwsza liczba to ' + numOne + ' a druga liczba to ' + numTwo +'.');
+    if(parseInt(numOne) % parseInt(numTwo) === 0){
+      res.send('Liczba B jest dzielnikiem liczby A.')
+    } else {
+      res.send('Liczba B nie jest dzielnikiem liczby A.')
+    }
+});
 
 app.listen(3000,()=>{
   console.log('Serwer działa na porcie 3000');
